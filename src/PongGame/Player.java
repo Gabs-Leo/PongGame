4e67aa13a91b7df5958d9d10cb304b802b6e032d
Gabs-Leo <gabrieldemoraistaveira@gmail.com>
2021-04-19 06:45:03 -0300

    Added Player
    Added player, movements and collisions.

--- src/PongGame/Player.java
+++ src/PongGame/Player.java
@@ -1,4 +1,37 @@
 package PongGame;
+import java.awt.Color;
+import java.awt.Graphics;
 
 public class Player {
+	
+	public boolean right, left;
+	public int x,y;
+	public int width, height;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 3;
+	}
+	
+	//Event Tick and Render//
+	public void eventTick(){
+		if(right){
+			x++;
+		}
+		else if(left) {
+			x--;
+		}
+		if(x+width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		}
+		else if(x < 0) {
+			x = 0;
+		}
+	}
+	public void render(Graphics g) {
+		g.setColor(new Color(255,255,255));
+		g.fillRect(x, y, width, height);
+	}
 }
